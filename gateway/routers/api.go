0d8eacc7941f1e4cab174764d616c5a09d0701dc
linhan.cai <linhan.cai@hi.com>
2019-06-14 16:41:50 +0800

    remove handy cat

--- gateway/routers/api.go
+++ gateway/routers/api.go
@@ -1,6 +1,7 @@
 package routers
 
 import (
+	"fmt"
 	"gateway/api/handlers"
 	"github.com/julienschmidt/httprouter"
 	"net/http"
@@ -11,8 +12,14 @@ import (
 
 func Api(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 
-	switch path := strings.Split(ps[0].Value, "/"); path[1] + "/" + path[2] {
-	case "healthcheck/":
+	path := strings.Split(ps[0].Value, "/");
+
+	profix := strings.Join(path[1:2], "/")
+	fmt.Printf("%v\n", path);
+	fmt.Println(profix)
+
+	switch  profix {
+	case "healthcheck":
 		handlers.HealthCheck(w, r)
 	case "ai/v1":
 		handlers.AiServices(w, r)
@@ -21,7 +28,7 @@ func Api(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	default:
 		public.Logger(public.Message{"msg": "Unexpect Request Path"})
 		//fmt.Fprintf("Unexpect Request Path")
-		http.Error(w, "Unexpected Request Path", 502)
+		http.Error(w, "Unexpected Request Path", 404)
 	}
 	return
 }
